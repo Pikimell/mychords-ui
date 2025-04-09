b8156ba3e050d9778327780f7afcc9e28d5d0917
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-04-09 16:52:30 +0300

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,18 +2,26 @@ import { useDispatch } from 'react-redux';
 import Navigation from '../../components/sections/Navigation/Navigation';
 import { setTitle } from '../../redux/meta/slice';
 import style from './HomePage.module.css';
+import { isAdminStatus } from '../../utils/initTelegram';
+import { Button } from 'antd';
 
 const HomePage = () => {
+  const isAdmin = isAdminStatus();
   const dispatch = useDispatch();
   dispatch(setTitle('Головне меню'));
   // const handleAdminClick = () => {
   //   localStorage.setItem('isAdmin', 'true');
   // };
+  const handleClear = () => {
+    localStorage.clear();
+    localStorage.setItem('isAdmin', true);
+  };
 
   return (
     <div className={style['page']}>
       <Navigation showMainPage={false} />
       {/* <button onClick={handleAdminClick}>ADMIN</button> */}
+      {isAdmin && <Button onClick={handleClear}>Clear Storage</Button>}
     </div>
   );
 };
